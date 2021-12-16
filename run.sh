e4469476b44cd1c979e9ae6ddd1ac400819baa57
Hagen Malessa <hagen.malessa2@dpdhl.com>
2021-12-16 14:34:56 +0100

    fix host

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[[ -n "${NGINXLDAP_HOSTNAME}" ]] && flags="--host ${NGINXLDAP_HOSTNAME}"
+flags="--host 0.0.0.0"
 [[ -n "${NGINXLDAP_PORT_NUMBER}" ]] && flags="${flags} -p ${NGINXLDAP_PORT_NUMBER}"
 [[ -n "${NGINXLDAP_LDAP_URI}" ]] && flags="${flags} -u ${NGINXLDAP_LDAP_URI}"
 [[ -n "${NGINXLDAP_LDAP_BASE_DN}" ]] && flags="${flags} -b ${NGINXLDAP_LDAP_BASE_DN}"
